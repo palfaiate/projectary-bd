08a993d9a094828ada90207e195ea2986f6ed47e
Paulo Alfaiate <syl.paulo.alfaiate@gmail.com>
2017-04-05 11:07:13 +0000

    Updated reposync.sh working 100%

--- reposync.sh
+++ reposync.sh
@@ -4,7 +4,5 @@ PROJECT='projectary-bd'
 git remote add upstream git@github.com:$ORGANIZATION/$PROJECT.git
 git stash
 git fetch upstream
-#git merge upstream/master master
-#git rebase upstream/master
 git pull upstream master
 git stash pop
