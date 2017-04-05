dad5d9b16b0100cd21b5da41ad6d156164a0ea29
Paulo Alfaiate <syl.paulo.alfaiate@gmail.com>
2017-04-05 10:20:39 +0000

    improved reposync.sh to be generic implemented $ORGANIZATION and $PROJECT variables

--- reposync.sh
+++ reposync.sh
@@ -1,3 +1,6 @@
-git remote add upstream git@github.com:iptomar/projectary-bd.git
+#!/bin/bash
+ORGANIZATION='iptomar'
+PROJECT='projectary-bd'
+git remote add upstream git@github.com:$ORGANIZATION/$PROJECT.git
 git fetch upstream
 git pull upstream master
