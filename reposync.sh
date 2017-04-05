4f7b98a635cf027ae7493a98aabb6d349902403c
Paulo Alfaiate <syl.paulo.alfaiate@gmail.com>
2017-04-05 11:05:59 +0000

    Extra Testing reposync.sh

--- reposync.sh
+++ reposync.sh
@@ -2,7 +2,9 @@
 ORGANIZATION='iptomar'
 PROJECT='projectary-bd'
 git remote add upstream git@github.com:$ORGANIZATION/$PROJECT.git
+git stash
 git fetch upstream
-git merge upstream/master master
-git rebase upstream/master
-#git pull upstream master
+#git merge upstream/master master
+#git rebase upstream/master
+git pull upstream master
+git stash pop
