247251f99e5a740f95d06c3bd5c3328ddf594b4f
Paulo Alfaiate <syl.paulo.alfaiate@gmail.com>
2017-04-05 11:03:12 +0000

    Testing reposync.sh

--- reposync.sh
+++ reposync.sh
@@ -5,4 +5,4 @@ git remote add upstream git@github.com:$ORGANIZATION/$PROJECT.git
 git fetch upstream
 git merge upstream/master master
 git rebase upstream/master
-git pull upstream master
+#git pull upstream master
