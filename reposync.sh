40a4aa21415e50e86d02b74ea24477503fa90ee1
Paulo Alfaiate <syl.paulo.alfaiate@gmail.com>
2017-04-05 11:01:23 +0000

    Added changes to reposync.sh

--- reposync.sh
+++ reposync.sh
@@ -3,4 +3,6 @@ ORGANIZATION='iptomar'
 PROJECT='projectary-bd'
 git remote add upstream git@github.com:$ORGANIZATION/$PROJECT.git
 git fetch upstream
+git merge upstream/master master
+git rebase upstream/master
 git pull upstream master
